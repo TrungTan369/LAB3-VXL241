25857738f7e5140d761bea15bbd81a51bdc16d9b
TrungTan <ttqpentakill@gmail.com>
2024-10-28 17:51:53 +0700

    99%

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -22,7 +22,6 @@
 /* Private includes ----------------------------------------------------------*/
 /* USER CODE BEGIN Includes */
 #include "global.h"
-
 /* USER CODE END Includes */
 
 /* Private typedef -----------------------------------------------------------*/
